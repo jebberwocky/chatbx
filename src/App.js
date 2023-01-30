e4ab21051c6ca59cb5516c5debb9a87ed0698f2a
Jeff Liu <jebberwocky@gmail.com>
2023-01-30 17:52:49 +0800

    x

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
+import React from "react";
+import Chatbot from "react-chatbot-kit";
+import { createChatBotMessage } from "react-chatbot-kit";
+import 'react-chatbot-kit/build/main.css';
 import './App.css';
+
+
+// MessageParser starter code
+class MessageParser {
+  constructor(actionProvider, state) {
+    this.actionProvider = actionProvider;
+    this.state = state;
+  }
+
+  parse(message) {
+    this.actionProvider.handleHello();
+    
+    console.log(message)
+  }
+}
+
+// ActionProvider starter code
+class ActionProvider {
+   constructor(
+    createChatBotMessage,
+    setStateFunc,
+    createClientMessage,
+    stateRef,
+    createCustomMessage
+  ) {
+    this.createChatBotMessage = createChatBotMessage;
+    this.setState = setStateFunc;
+    this.createClientMessage = createClientMessage;
+    this.stateRef = stateRef;
+    this.createCustomMessage = createCustomMessage;
+  }
+  handleHello = () => {
+    console.log()
+    const botMessage = createChatBotMessage('I\'m empty mind');
+    this.setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, botMessage],
+    }));
+  };
+
+}
+// Config starter code
+
+const config = {
+  initialMessages: [createChatBotMessage(`I'm empty mind`)]
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
     </div>
   );
 }
