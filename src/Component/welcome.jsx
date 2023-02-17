c2ca74a642662d43a389ec4aa3c50af309908cc5
Jeff Liu <jebberwocky@gmail.com>
2023-02-17 18:45:52 +0800

    update

---
+++ src/Component/welcome.jsx
@@ -0,0 +1,18 @@
+// new file called DogPicture.jsx
+import React, { useEffect, useState } from 'react';
+
+const Welcome = () => {
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {}, []);
+
+  return (
+    <div class="react-chatbot-kit-chat-bot-message-container">
+        <div class="react-chatbot-kit-chat-bot-avatar">
+            <div class="react-chatbot-kit-chat-bot-avatar-container"><p class="react-chatbot-kit-chat-bot-avatar-letter">🙊</p></div></div><div class="react-chatbot-kit-chat-bot-message">
+        <span>为保护您的私隐,请勿任何泄露个人信息. 🐵不会收集任何个人信息</span><div class="react-chatbot-kit-chat-bot-message-arrow"></div>
+        </div></div>
+  );
+};
+
+export default Welcome;
