120481cbb26680d9addb02c6f775476580e6a178
jeff <jebberwocky@gmail.com>
2024-03-07 16:38:04 +0800

    update intro

--- src/Component/welcome.jsx
+++ src/Component/welcome.jsx
@@ -12,12 +12,10 @@ const Welcome = () => {
         <span>保护您的私隐,请勿任何泄露个人信息. 🐵不会收集任何个人信息. 欢迎联系:
             <a href="mailto:themonkey0220@gmail.com">themonkey0220@gmail.com</a></span>
         <div>
-            <span>shortcut:</span>
-            <ul><li>生成图片 showmethepic:生成一张X图</li>
-                <li>图片识别 uploadpic:</li>
-                <li>识别热量 kcalthis:</li>
-                <li>转语音 say:</li>
-                <li>求一卦 =&gt; 算卦</li>
+            <span>试试看输入以下:</span>
+            <ul><li>showmethepic:生成一张小黄鸭图</li>
+                <li>say:一无所有不好吗?</li>
+                <li>算卦</li>
             </ul>
         </div>
         <div class="react-chatbot-kit-chat-bot-message-arrow"></div>
